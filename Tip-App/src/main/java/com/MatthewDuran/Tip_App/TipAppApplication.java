9570d49990f17eb4cd7b89db6fa10a0c392b9efb
Matthew Duran <68670144+Matthew-Duran@users.noreply.github.com>
2025-09-05 13:00:15 -0400

    Add files via upload

---
+++ Tip-App/src/main/java/com/MatthewDuran/Tip_App/TipAppApplication.java
@@ -0,0 +1,13 @@
+package com.MatthewDuran.Tip_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipAppApplication.class, args);
+	}
+
+}
